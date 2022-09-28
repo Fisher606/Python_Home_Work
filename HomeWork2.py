8ef336a8921835fb55c3d3499fa897e37e771909
Vadim <fishermanvadik@gmail.com>
2022-09-28 17:57:26 +0300

    Задача 1

--- HomeWork2.py
+++ HomeWork2.py
@@ -1,8 +1,18 @@
-str1=str(input())
-print(str1)
-k=0
-for elem in str1:
-    if elem.isdigit():
-        k += int(elem)
-        print(elem)
-print("k=" ,k)
+# # Задача 1.  Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+
+# # Пример:
+
+# # - 6782 -> 23
+# # - 0,56 -> 11
+
+
+# str1=str(input())
+# print(str1)
+# k=0
+# for elem in str1:
+#     if elem.isdigit():
+#         k += int(elem)
+#         print(elem)
+# print(f' - {str1} ->' ,k)
+
+
